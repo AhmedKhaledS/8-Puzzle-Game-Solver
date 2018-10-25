a4213ace53380398bb0ed1ddac916c41adee8368
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-10-25 16:26:40 +0200

    Added simple GUI for 8-puzzle-game and initialized base gui classes

---
+++ graphicsDisplay.py
@@ -0,0 +1,53 @@
+import pygame as pg
+import tile
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 480, 480
+TILE_WIDTH = WINDOW_WIDTH / 3
+TILE_HEIGHT = WINDOW_HEIGHT / 3
+x, y = 0, 0
+
+
+def initialize_game():
+    pg.init()
+    window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pg.display.set_caption("8-Puzzle-Game")
+    vel = 5
+    return window
+
+
+def prepare_tiles():
+    x, y = 0, 0
+    tiles = []
+    for i in range(8):
+        tiles.append(tile.Tile(x, y, TILE_WIDTH, TILE_HEIGHT, i+1))
+        x = (x + TILE_WIDTH + 3 if x + TILE_WIDTH < WINDOW_WIDTH else 0)
+        if (i+1) % 3 == 0:
+            y = (y + TILE_HEIGHT + 3 if y + TILE_HEIGHT < WINDOW_HEIGHT else 0)
+
+    return tiles
+
+
+def draw_tiles(window, tiles):
+    font = pg.font.SysFont('Comic Sans MS', 120)
+    for i in range(8):
+        pg.draw.rect(window, (87, 89, 91),
+                     (tiles[i].pos_x, tiles[i].pos_y, tiles[i].width, tiles[i].height))
+        text_tile = font.render(str(tiles[i].number), False, (16, 39, 76))
+        window.blit(text_tile, (tiles[i].pos_x + TILE_WIDTH / 3, tiles[i].pos_y + TILE_HEIGHT / 4))
+    pg.display.update()
+
+def main():
+    window = initialize_game()
+    run = True
+    while run:
+        pg.time.delay(100)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        tiles = prepare_tiles()
+        draw_tiles(window, tiles)
+
+    pg.quit()
+
+if __name__ == '__main__':
+    main()
---
+++ tile.py
@@ -0,0 +1,52 @@
+
+class Tile:
+
+    def __init__(self, pos_x, pos_y, width, height, number):
+        self._pos_x = pos_x
+        self._pos_y = pos_y
+        self._width = width
+        self._height = height
+        self._number = number
+
+    @property
+    def pos_x(self):
+        return self._pos_x
+
+    @pos_x.setter
+    def pos_x(self, value):
+        self._pos_x = value
+
+    @property
+    def pos_y(self):
+        return self._pos_y
+
+    @pos_y.setter
+    def pos_y(self, value):
+        self._pos_y = value
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def number(self):
+        return self._number
+
+    @number.setter
+    def number(self, value):
+        self._number = value
+
+    def get_properties(self):
+        return (self.pos_x, self.pos_y, self.width, self.height, self.number)
