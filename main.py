1fa72c736f41b3305ddd81aafa6da08ddda8a7da
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-10-26 15:09:06 +0200

    Added the concrete classes for the flow of the search algorithm of the game.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from gui.graphicsDisplay import GraphicsPuzzle
-
+from gameState import EightPuzzleState
+import searchStrategy
 
 def main():
-    puzzle_game = GraphicsPuzzle()
-    puzzle_game.start()
+    # puzzle_game = GraphicsPuzzle()
+    # puzzle_game.start()
 
+    puzzle = EightPuzzleState()
+    search = searchStrategy.Search()
 
 if __name__ == '__main__':
     main()
---
+++ searchStrategy.py
@@ -0,0 +1,14 @@
+
+class Search:
+
+    def breadth_first_search(self, gameState, frontier):
+        pass
+
+    def depth_first_search(self, gameState, frontier):
+        pass
+
+    def a_star_search(self, gameState, frontier):
+        pass
+
+    def generic_graph_search(self, gameState, frontier):
+        pass
